8cb5569121a373132e9f176369b20f7b42d7079d
AleksandraKasznia <33521222+AleksandraKasznia@users.noreply.github.com>
2018-03-18 21:47:15 +0100

    [master] Repair test.

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,10 +4,5 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  if (value <= -13 or value >= 13) return 0;
-  if (value == 0) return 1;
-  else {
-    if (value < 0) return value * factorial(value + 1);
-    else return value * factorial(value - 1);
-  }
+  return 0;
 }
